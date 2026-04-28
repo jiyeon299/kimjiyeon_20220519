1c0d0a2913a2b469dd342f72443d855407927167
Jiyeon Kim <jjy31029@naver.com>
2026-04-28 17:58:44 +0900

    4

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
-  // 커밋 확인 !! 
-  // 커밋 확인 두번째 
+  createCanvas(600,600);
+  
+  
 }
 
 function draw() {
   background(220);
+
+  fill(255, 255, 0);
+  noStroke();
+  arc(0,0,40,40, PI/ 5, PI / 6, PIE);
+  
 }
+
+
