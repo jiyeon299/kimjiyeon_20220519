7e4779522937eb09afdf97c6140c8615f12ac254
Jiyeon Kim <jjy31029@naver.com>
2026-04-29 15:25:33 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
 let x = 50;
 let y = 50;
 let angle = 3;
-let speed = 2;
+let speed = 3;
 
+let pacmanSize = 50;
 let mapImg;
 
+let walls = [
+  { x: 100, y: 100, w: 200, h: 20 },
+  { x: 300, y: 200, w: 20, h: 200 },
+  { x: 500, y: 300, w: 150, h: 20 },
+];
+
 function preload() {
-  mapImg = loadImage('Map.png');
+  mapImg = loadImage("Map.png");
 }
 
 function setup() {
@@ -41,21 +48,45 @@ function draw() {
   } 
 
   // 이동 
-  x += dirX * speed;
-  y += dirY * speed;
+  let nextX = x + dirX * speed;
+  let nextY = y + dirY * speed;
+
+  // 벽 충돌 체크
+  if (!hitWall(nextX, nextY)) {
+    x = nextX;
+    y = nextY;
+  }
 
   // 팩맨 
   fill(255, 255, 0);
   noStroke();
+  
   let mouth = abs(sin(frameCount * 0.1)) * PI / 5;
 
   push();
   translate(x,y);
   rotate(angle);
-  arc(0,0, 80, 80, mouth, TWO_PI - mouth, PIE);
+  arc(0,0, pacmanSize, pacmanSize, mouth, TWO_PI - mouth, PIE);
   pop();
 }
 
 
+function hitWall(hx, hy) {
+  let r = pacmanSize / 2;
+
+  for (let i = 0; i < walls.length; i++) {
+    let wall = walls[i];
+
+    if (
+      hx + r > wall.x &&
+      hx - r < wall.x + wall.w &&
+      hy + r > wall.y &&
+      hy - r < wall.y + wall.h
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
